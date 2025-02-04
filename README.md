c1e9d3f19769049eb49032eada5be54781aa404a
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-04 20:01:14 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Structures & Algorithms Grinding
+
+Solutions to dsa challenges on neetcode, leetcode, hackerrank, etc.
---
+++ ds/src/main/java/com/diegolopes98/dsa/ds/Main.java
@@ -0,0 +1,7 @@
+package com.diegolopes98.dsa.ds;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
