22ca6f66dbb5ceb8191c9c34766e436f24eee55e
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-04 20:41:36 -0300

    feat(neetcode): add ContainsDuplicate solution

---
+++ neetcode/src/main/java/com/diegolopes98/dsa/neetcode/ArraysAndHashing/ContainsDuplicate/ContainsDuplicate.java
@@ -0,0 +1,5 @@
+package com.diegolopes98.dsa.neetcode.ArraysAndHashing.ContainsDuplicate;
+
+public interface ContainsDuplicate {
+    boolean hasDuplicate(int[] nums);
+}
---
+++ neetcode/src/main/java/com/diegolopes98/dsa/neetcode/ArraysAndHashing/ContainsDuplicate/ContainsDuplicateImpl.java
@@ -0,0 +1,22 @@
+package com.diegolopes98.dsa.neetcode.ArraysAndHashing.ContainsDuplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicateImpl implements ContainsDuplicate {
+
+    @Override
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for(int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
