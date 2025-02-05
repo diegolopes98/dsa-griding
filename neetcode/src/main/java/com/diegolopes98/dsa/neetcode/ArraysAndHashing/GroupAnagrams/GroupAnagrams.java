23a20d353336fa9ce037f8743eacc8ea0761bea6
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-05 00:04:05 -0300

    feat(neetcode): add GroupAnagrams solution

---
+++ neetcode/src/main/java/com/diegolopes98/dsa/neetcode/ArraysAndHashing/GroupAnagrams/GroupAnagrams.java
@@ -0,0 +1,7 @@
+package com.diegolopes98.dsa.neetcode.ArraysAndHashing.GroupAnagrams;
+
+import java.util.List;
+
+public interface GroupAnagrams {
+    List<List<String>> groupAnagrams(String[] strs);
+}
---
+++ neetcode/src/main/java/com/diegolopes98/dsa/neetcode/ArraysAndHashing/GroupAnagrams/GroupAnagramsImpl.java
@@ -0,0 +1,26 @@
+package com.diegolopes98.dsa.neetcode.ArraysAndHashing.GroupAnagrams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagramsImpl implements GroupAnagrams{
+    @Override
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+
+        return new ArrayList<>(res.values());
+    }
+}
