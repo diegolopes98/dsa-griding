113b132637d4c7e92a1a483a7b817291c0215653
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-04 21:16:41 -0300

    feat(neetcode): add ValidAnagram solution

---
+++ neetcode/src/main/java/com/diegolopes98/dsa/neetcode/ArraysAndHashing/ValidAnagram/ValidAnagram.java
@@ -0,0 +1,5 @@
+package com.diegolopes98.dsa.neetcode.ArraysAndHashing.ValidAnagram;
+
+public interface ValidAnagram {
+    boolean isAnagram(String s, String t);
+}
---
+++ neetcode/src/main/java/com/diegolopes98/dsa/neetcode/ArraysAndHashing/ValidAnagram/ValidAnagramImpl.java
@@ -0,0 +1,34 @@
+package com.diegolopes98.dsa.neetcode.ArraysAndHashing.ValidAnagram;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ValidAnagramImpl implements ValidAnagram {
+
+    @Override
+    public boolean isAnagram(String s, String t) {
+        Objects.requireNonNull(s, "Param 's' of isAnagram must be non null");
+        Objects.requireNonNull(t, "Param 't' of isAnagram must be non null");
+
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> sOccurrences = getCharacterOccurrences(s);
+        Map<Character, Integer> tOccurrences = getCharacterOccurrences(t);
+
+        return sOccurrences.equals(tOccurrences);
+    }
+
+    private Map<Character, Integer> getCharacterOccurrences(String s) {
+        HashMap<Character, Integer> occurrences = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            occurrences.put(
+                    s.charAt(i),
+                    occurrences.getOrDefault(s.charAt(i), 0) + 1
+            );
+        }
+
+        return occurrences;
+    }
+}
