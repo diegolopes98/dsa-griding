7991ffa406f02bd8d95b9c70de974c83393e19f6
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-06 22:37:19 -0300

    test(leetcode): add RemoveDuplicatesFromSortedArray solution

---
+++ leetcode/src/test/java/com/diegolopes98/dsa/leetcode/Array/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,48 @@
+package com.diegolopes98.dsa.leetcode.Array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import java.util.Arrays;
+import java.util.List;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class RemoveDuplicatesFromSortedArrayTest {
+
+    RemoveDuplicatesFromSortedArray solution;
+
+    @BeforeAll
+    void setup() {
+        solution = new RemoveDuplicatesFromSortedArrayImpl();
+    }
+
+    @Test
+    void givenValidArrayWith1DuplicateAnd3Elements_whenCallingRemoveDuplicates_shouldReturn2() {
+        final var givenNumbers = new int[]{1, 1, 2};
+        final var expectedOutput = 2;
+        final var expectedNumbers = new int[]{1, 2};
+
+        final var actualOutput = solution.removeDuplicates(givenNumbers);
+        final var actualNumbers = Arrays.copyOfRange(givenNumbers, 0, expectedOutput);
+
+        Assertions.assertEquals(expectedOutput, actualOutput);
+        Assertions.assertArrayEquals(expectedNumbers, actualNumbers);
+    }
+
+    @Test
+    void givenValidArrayWith5DuplicateAnd10Elements_whenCallingRemoveDuplicates_shouldReturn2() {
+        final var givenNumbers = new int[]{0,0,1,1,1,2,2,3,3,4};
+        final var expectedOutput = 5;
+        final var expectedNumbers = new int[]{0,1,2,3,4};
+
+        final var actualOutput = solution.removeDuplicates(givenNumbers);
+        final var actualNumbers = Arrays.copyOfRange(givenNumbers, 0, expectedOutput);
+
+        Assertions.assertEquals(expectedOutput, actualOutput);
+        Assertions.assertArrayEquals(expectedNumbers, actualNumbers);
+    }
+
+}
