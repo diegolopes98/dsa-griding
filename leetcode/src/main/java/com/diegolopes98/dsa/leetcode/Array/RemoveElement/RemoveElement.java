30ab81707b432959ffd98e34fa996be7ee3b6bb8
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-06 23:45:58 -0300

    feat(leetcode): add RemoveElement solution

---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/RemoveElement/RemoveElement.java
@@ -0,0 +1,5 @@
+package com.diegolopes98.dsa.leetcode.Array.RemoveElement;
+
+public interface RemoveElement {
+    int removeElement(int[] nums, int val);
+}
---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/RemoveElement/RemoveElementImpl.java
@@ -0,0 +1,15 @@
+package com.diegolopes98.dsa.leetcode.Array.RemoveElement;
+
+public class RemoveElementImpl implements RemoveElement{
+    @Override
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
