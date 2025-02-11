d2ce527bd98f011846d17f14f4b2190bdb88df03
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-11 18:06:45 -0300

    feat(leetcode): add ConcatenationOfArray solution

---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/ConcatenationOfArray/ConcatenationOfArray.java
@@ -0,0 +1,5 @@
+package com.diegolopes98.dsa.leetcode.Array.ConcatenationOfArray;
+
+public interface ConcatenationOfArray {
+    int[] getConcatenation(int[] nums);
+}
---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/ConcatenationOfArray/ConcatenationOfArrayImpl.java
@@ -0,0 +1,16 @@
+package com.diegolopes98.dsa.leetcode.Array.ConcatenationOfArray;
+
+public class ConcatenationOfArrayImpl implements ConcatenationOfArray {
+    @Override
+    public int[] getConcatenation(int[] nums) {
+        final int l = nums.length;
+        int[] numsConcat = new int[l * 2];
+
+        for (int i = 0; i < l; i++) {
+            numsConcat[i] = nums[i];
+            numsConcat[i + l] = nums[i];
+        }
+
+        return numsConcat;
+    }
+}
