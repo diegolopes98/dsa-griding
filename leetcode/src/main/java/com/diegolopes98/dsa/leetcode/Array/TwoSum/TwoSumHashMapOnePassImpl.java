5492f0263c3fe8fbe04f555e5d978e379ec665c7
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-11 18:25:57 -0300

    feat(leetcode): add TwoSum solution using hash and one pass

---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/TwoSum/TwoSumHashMapOnePassImpl.java
@@ -0,0 +1,25 @@
+package com.diegolopes98.dsa.leetcode.Array.TwoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumHashMapOnePassImpl implements TwoSum{
+
+    @Override
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> comps = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (comps.containsKey(diff)) {
+                return new int[] { comps.get(diff), i };
+            }
+
+            comps.put(num, i);
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
