535ab0a25641b4df576a4b21ff1592c4f693c7db
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-11 19:41:29 -0300

    feat(leetcode): add PlusOne solution

---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/PlusOne/PlusOne.java
@@ -0,0 +1,5 @@
+package com.diegolopes98.dsa.leetcode.Array.PlusOne;
+
+public interface PlusOne {
+    int[] plusOne(int[] digits);
+}
---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/PlusOne/PlusOneImpl.java
@@ -0,0 +1,19 @@
+package com.diegolopes98.dsa.leetcode.Array.PlusOne;
+
+public class PlusOneImpl implements PlusOne{
+    @Override
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+                continue;
+            }
+            digits[i] += 1;
+            return digits;
+        }
+
+        int[] newDigits = new int[digits.length+1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+}
