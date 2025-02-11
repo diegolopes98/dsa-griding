b354d2a5e25897928b090af4fb338ec47556d394
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-11 19:17:10 -0300

    feat(leetcode): add SearchInsertPosition solution

---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/SearchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,5 @@
+package com.diegolopes98.dsa.leetcode.Array.SearchInsertPosition;
+
+public interface SearchInsertPosition {
+    int searchInsert(int[] nums, int target);
+}
---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/SearchInsertPosition/SearchInsertPositionImpl.java
@@ -0,0 +1,22 @@
+package com.diegolopes98.dsa.leetcode.Array.SearchInsertPosition;
+
+public class SearchInsertPositionImpl implements SearchInsertPosition {
+    @Override
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            final int curr = nums[mid];
+            if (curr == target) {
+                return mid;
+            } else if (curr < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
