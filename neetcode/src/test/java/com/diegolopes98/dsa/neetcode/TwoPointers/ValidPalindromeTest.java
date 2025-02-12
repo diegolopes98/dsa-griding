36d7d158e7a79a8c6f8abf6857c8d7dd08a66717
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-02-11 22:14:50 -0300

    test(neetcode): add ValidPalindrome solution

---
+++ neetcode/src/test/java/com/diegolopes98/dsa/neetcode/TwoPointers/ValidPalindromeTest.java
@@ -0,0 +1,63 @@
+package com.diegolopes98.dsa.neetcode.TwoPointers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class ValidPalindromeTest {
+
+    private ValidPalindrome solution;
+
+    @BeforeAll
+    void setup() {
+        solution = new ValidPalindromeImpl();
+    }
+
+    @Test
+    void givenAValidAlphaNumPalindrome_whenCallingIsPalindrome_shouldReturnTrue(){
+        final var givenSupposedPalindrome = "Was it a car or a cat I saw?";
+
+        final var actualOutput = solution.isPalindrome(givenSupposedPalindrome);
+
+        Assertions.assertTrue(actualOutput);
+    }
+
+    @Test
+    void givenAnInvalidAlphaNumPalindrome_whenCallingIsPalindrome_shouldReturnTrue(){
+        final var givenSupposedPalindrome = "tab a cat";
+
+        final var actualOutput = solution.isPalindrome(givenSupposedPalindrome);
+
+        Assertions.assertFalse(actualOutput);
+    }
+
+    @Test
+    void givenAnEmptyPalindrome_whenCallingIsPalindrome_shouldReturnTrue(){
+        final var givenSupposedPalindrome = "";
+
+        final var actualOutput = solution.isPalindrome(givenSupposedPalindrome);
+
+        Assertions.assertTrue(actualOutput);
+    }
+
+    @Test
+    void givenASpaceStringPalindrome_whenCallingIsPalindrome_shouldReturnTrue(){
+        final var givenSupposedPalindrome = " ";
+
+        final var actualOutput = solution.isPalindrome(givenSupposedPalindrome);
+
+        Assertions.assertTrue(actualOutput);
+    }
+
+    @Test
+    void givenASpecialCharsStringPalindrome_whenCallingIsPalindrome_shouldReturnTrue(){
+        final var givenSupposedPalindrome = " !+-_!@#$%*()}{][";
+
+        final var actualOutput = solution.isPalindrome(givenSupposedPalindrome);
+
+        Assertions.assertTrue(actualOutput);
+    }
+}
